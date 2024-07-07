c196d7d6581e1fcff19abe4366cd88b3056ec194
Alex Naumov <algena75@yandex.ru>
2024-07-07 19:24:44 +0300

    postgres in workflow att.6

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -70,11 +70,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("POSTGRES_NAME"),
-        "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "NAME": os.getenv("POSTGRES_NAME", 'postgres'),
+        "USER": os.getenv("POSTGRES_USER", 'postgres'),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", 'postgres'),
         "HOST": os.getenv("POSTGRES_HOST", '127.0.0.1'),
-        "PORT": os.getenv("POSTGRES_PORT"),
+        "PORT": os.getenv("POSTGRES_PORT", 5432),
     }
 }
 
